c9a0d4077be901d909ac02ffc747b57a4d4e031a
anubhootipandey <anubhootipandey05@gmail.com>
2024-12-19 23:09:53 +0530

    added links to the social icons

--- app/dashboard/socials/page.js
+++ app/dashboard/socials/page.js
@@ -7,43 +7,33 @@ import {
   Globe,
   Terminal,
 } from "lucide-react";
-import { SiHackerrank, SiLeetcode, SiGeeksforgeeks } from "react-icons/si";
+import { SiHackerrank, SiLeetcode } from "react-icons/si";
 
 const SocialGlobe = () => {
 
   return (
-    <div 
-    style={{
-        backgroundImage: `url('/images/pic)`,
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-      }}
+    <div
     className="h-screen flex text-white items-center justify-center ">
-        <div className="mt-10">
-          <div className="grid grid-cols-3 border-2 border-white p-8 rounded-lg gap-10">
-            <a href="https://github.com/" target="_blank" rel="noopener noreferrer">
-              <Github className="h-[100px] w-[100px]" />
+          <div className="grid grid-cols-3 p-8 gap-10">
+            <a href="https://github.com/anubhootipandey/" className="bg-gray-900 p-8 rounded-full" target="_blank" rel="noopener noreferrer">
+              <Github className="h-[70px] w-[70px]" />
             </a>
-            <a href="https://linkedin.com/" target="_blank" rel="noopener noreferrer">
+            <a href="https://www.linkedin.com/in/anubhooti-pandey-418063228/" className="bg-gray-900 p-8 rounded-full" target="_blank" rel="noopener noreferrer">
               <Linkedin className="h-[70px] w-[70px]" />
             </a>
-            <a href="https://yourblog.com" target="_blank" rel="noopener noreferrer">
+            <a href="https://medium.com/@anubhootipandey/" className="bg-gray-900 p-8 rounded-full" target="_blank" rel="noopener noreferrer">
               <Globe className="h-[70px] w-[70px]" />
             </a>
-            <a href="https://www.hackerrank.com/" target="_blank" rel="noopener noreferrer">
+            <a href="https://www.hackerrank.com/profile/anubhootipandey1/" className="bg-gray-900 p-8 rounded-full" target="_blank" rel="noopener noreferrer">
               <SiHackerrank className="h-[70px] w-[70px]" />
             </a>
-            <a href="https://leetcode.com/" target="_blank" rel="noopener noreferrer">
+            <a href="https://leetcode.com/u/ap-05//" className="bg-gray-900 p-8 rounded-full" target="_blank" rel="noopener noreferrer">
               <SiLeetcode className="h-[70px] w-[70px]" />
             </a>
-            <a href="https://www.geeksforgeeks.org/" target="_blank" rel="noopener noreferrer">
-              <SiGeeksforgeeks className="h-[70px] w-[70px]" />
-            </a>
-            <a href="https://www.codingninjas.com/" target="_blank" rel="noopener noreferrer">
+            <a href="https://www.naukri.com/code360/profile/d9f25dc0-b782-4269-ac04-946b3d1f860f/" className="bg-gray-900 p-8 rounded-full" target="_blank" rel="noopener noreferrer">
               <Terminal className="h-[70px] w-[70px]" />
             </a>
           </div>
-        </div>
     </div>
   );
 };
